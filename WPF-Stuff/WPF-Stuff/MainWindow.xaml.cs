5607ae3f65cd86ca1c816ee24d3ba6de6439914f
Rob Bell <rob.bell.86@gmail.com>
2016-05-10 21:37:19 -0400

    Created SingleRowUsingOnlyCode and date picker that is limited to only Friday (it is super slow right now)

--- WPF-Stuff/WPF-Stuff/MainWindow.xaml.cs
+++ WPF-Stuff/WPF-Stuff/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -7,15 +8,51 @@ namespace WPF_Stuff
 {
     public partial class MainWindow : Window
     {
+        private readonly SingleRowUsingOnlyCode _singleRowUsingOnlyCode;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _singleRowUsingOnlyCode = new SingleRowUsingOnlyCode(40);
+            _singleRowUsingOnlyCode.SingleRowGrid.SetValue(Grid.RowProperty, 0);
+            MainGridPanel.Children.Add(_singleRowUsingOnlyCode.SingleRowGrid);
+
+            BlackOutDates();
+        }
+
+        private void BlackOutDates()
+        {
+            DateTime minDate = FridayPicker.DisplayDateStart ?? DateTime.MinValue;
+            DateTime maxDate = FridayPicker.DisplayDateEnd ?? DateTime.MaxValue;
+
+            for (DateTime date = minDate; date <= maxDate && ThereIsAtLeastSevenMoreDaysLeftBeforeMaxValue(date); date = date.AddDays(1))
+            {
+                if (date.DayOfWeek != DayOfWeek.Friday)
+                {
+                    FridayPicker.BlackoutDates.Add(new CalendarDateRange(date));
+                }
+            }
+        }
+
+        private static bool ThereIsAtLeastSevenMoreDaysLeftBeforeMaxValue(DateTime date)
+        {
+            return (DateTime.MaxValue - date.AddDays(1)).Days > 1;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            TheSingleRow.AddColumn();
+            int result;
+            if(int.TryParse(ColumnIndexTextBox.Text, out result))
+            {
+                _singleRowUsingOnlyCode.HighlightColumn(result);
+            }
+        }
+
+        private void ColumnIndexTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs eventArgs)
+        {
+            int result;
+            eventArgs.Handled = !int.TryParse(eventArgs.Text, out result);
         }
     }
 }
---
+++ WPF-Stuff/WPF-Stuff/SingleRowUsingOnlyCode.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WPF_Stuff
+{
+    public class SingleRowUsingOnlyCode
+    {
+        public Grid SingleRowGrid = new Grid
+        {
+            Height = 25,
+            Margin = new Thickness(0),
+            VerticalAlignment = VerticalAlignment.Center
+        };
+
+        private static readonly Color CellFillColor = Colors.Blue;
+
+        public SingleRowUsingOnlyCode(int numberOfColumns)
+        {
+            InitializeGrid(numberOfColumns);
+        }
+
+        private void InitializeGrid(int numberOfColumns)
+        {
+            for (int i = 0; i < numberOfColumns; i++)
+            {
+                AddColumnToGrid();
+            }
+        }
+
+        private void AddColumnToGrid()
+        {
+            SingleRowGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            int columnIndex = SingleRowGrid.Children.Count;
+
+            Rectangle cell = new Rectangle
+            {
+                Stroke = new SolidColorBrush(Colors.Black),
+                Fill = new SolidColorBrush(CellFillColor)
+            };
+
+            cell.SetValue(Grid.ColumnProperty, columnIndex);
+            SingleRowGrid.Children.Add(cell);
+        }
+
+        public void HighlightColumn(int columnIndex)
+        {
+            UIElementCollection uiElementCollection = SingleRowGrid.Children;
+            foreach (Rectangle rectangle in uiElementCollection.OfType<Rectangle>())
+            {
+                int rectanglesColumnIndex = -1;
+                int.TryParse(rectangle.GetValue(Grid.ColumnProperty).ToString(), out rectanglesColumnIndex);
+
+                Color fillColor = CellFillColor;
+
+                if (rectanglesColumnIndex == columnIndex)
+                {
+                    fillColor = Colors.Yellow;
+                }
+
+                rectangle.Fill = new SolidColorBrush(fillColor);
+            }
+        }
+    }
+}
